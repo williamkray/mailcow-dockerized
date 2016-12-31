54e7c89c10fba64564499ea977c4ae171e0af199
William Kray <william@kray.ca>
2016-12-31 09:32:33 -0800

    revert to default nginx container since my watcher doesn't work anyway

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-pushd ./data/Dockerfiles/nginx/
-  docker build -t nginx-mailcow:sslwatch .
-popd
+#pushd ./data/Dockerfiles/nginx/
+#  docker build -t nginx-mailcow:sslwatch .
+#popd
 
 docker-compose up -d
